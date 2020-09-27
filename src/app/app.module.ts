d4ed7fb76354d636e24f061d1d8096b568d3b2b0
amineb01 <bellarej.amine@gmail.com>
2020-09-27 20:11:50 +0100

    init project && side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './templates/sidenav/sidenav.component';
+
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
+
   ],
   imports: [
+    BrowserAnimationsModule,
+    AngularMaterialModule,
     BrowserModule
   ],
   providers: [],
